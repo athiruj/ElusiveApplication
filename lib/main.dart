67e2d0ac9b26d0d8d3a9d2239b4ea03475f44427
Athiruj <athirujkaewseesuk@gmail.com>
2023-12-18 10:38:54 +0700

    setup ColorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ///
+        //? Look for ColorScheme in https://figma.fun/Ed3oB5
+        ///
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF951519),
+          primaryContainer: Color(0xFF851114),
+          onPrimary: Color(0xFF5A0D0F),
+          onPrimaryContainer: Color(0xFF370809),
+          secondary: Color(0xFFF1B326),
+          secondaryContainer: Color(0xFFE6A50F),
+          onSecondary: Color(0xFFC38B0D),
+          onSecondaryContainer: Color(0xFF9F720A),
+          tertiary: Color(0xFF76B100),
+          tertiaryContainer: Color(0xFF6DA300),
+          onTertiary: Color(0xFF5B8900),
+          onTertiaryContainer: Color(0xFF486B00),
+        ),
         useMaterial3: false,
-        //
-        // Look for TextTheme design in https://figma.fun/B4g5b5
-        //
+
+        ///
+        //? Look for TextTheme design in https://figma.fun/B4g5b5
+        ///
         fontFamily: "Poppins",
         textTheme: const TextTheme(
           titleLarge: TextStyle(
@@ -57,16 +74,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -78,50 +85,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -138,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
